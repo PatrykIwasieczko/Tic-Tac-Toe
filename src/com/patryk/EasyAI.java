135f55e0c66234516f5035f527f82dbe5f6d93ab
Patryk <patryk.iwasieczko@gmail.com>
2020-08-11 16:02:02 +0200

    Add easyAI class

--- src/com/patryk/EasyAI.java
+++ src/com/patryk/EasyAI.java
@@ -1,4 +1,21 @@
 package com.patryk;
 
-public class EasyAI {
+import java.util.ArrayList;
+
+public class EasyAI extends Player {
+
+    private Field field;
+
+    EasyAI(Game game) {
+        super(game);
+        field = game.getField();
+    }
+
+    @Override
+    protected Coordinates getMove() {
+        ArrayList<Coordinates> emptyCoordinates = field.getEmptyFields();
+        Coordinates result = emptyCoordinates.get((int)(Math.random() * emptyCoordinates.size()));
+        System.out.println("Making move level \"easy\"");
+        return result;
+    }
 }
