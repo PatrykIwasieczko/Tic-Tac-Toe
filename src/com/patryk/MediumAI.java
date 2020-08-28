7cfc068e8aa714105bb724c9882b08b1b12214ea
Patryk <patryk.iwasieczko@gmail.com>
2020-08-28 16:35:45 +0200

    Add medium AI

--- src/com/patryk/MediumAI.java
+++ src/com/patryk/MediumAI.java
@@ -1,4 +1,44 @@
 package com.patryk;
 
-public class MediumAI {
+import java.util.ArrayList;
+
+public class MediumAI extends Player {
+
+    Field field;
+
+    public MediumAI(Game game) {
+        super(game);
+        field = game.getField();
+    }
+
+    @Override
+    protected Coordinates getMove() {
+        Symbol enemyPlayer = humanPlayer == Symbol.X ? Symbol.O : Symbol.X;
+        System.out.println("Making move level \"medium\"");
+        Coordinates enemyWinnerMove = null;
+
+        //Get possible moves
+        ArrayList<Coordinates> emptyCoordinates = field.getEmptyFields();
+        for (Coordinates currentMove : emptyCoordinates) {
+            Field newField = new Field(field.getField());
+            newField.setCell(currentMove, humanPlayer);
+
+            if (newField.getWinner() == humanPlayer) {
+                return currentMove;
+            }
+
+            newField.setCell(currentMove, enemyPlayer);
+
+            if (newField.getWinner() == enemyPlayer) {
+                enemyWinnerMove = currentMove;
+            }
+        }
+
+        if (enemyWinnerMove != null) {
+            return enemyWinnerMove;
+        }
+
+        return emptyCoordinates.get((int)(Math.random() * emptyCoordinates.size()));
+
+    }
 }
